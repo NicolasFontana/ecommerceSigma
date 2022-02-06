c81acc63b53ded8b010acdcdd1d728bf3bae76f2
NicolasFontana <nico.fontana12@gmail.com>
2022-02-06 18:26:56 -0300

    Corrijo validación de contactos

--- js/validacion.js
+++ js/validacion.js
@@ -1,11 +1,11 @@
 // Nodos
 
+let formNombre = document.getElementById('formNombre');
 let formNumero = document.getElementById('formNumero');
 let formEmail = document.getElementById('formEmail');
 let formTexto = document.getElementById('formTexto');
 let formCheckbox = document.getElementById('formCheckbox');
 let formEnviar = document.getElementById('formEnviar');
-let formNombre = document.getElementById('formNombre');
 let resultado = document.getElementById('resultado');
 
 // Validar
@@ -14,7 +14,7 @@ function validar () {
   // validar nombre
   if ((formNombre.value == "") || (formNombre.value.length < 3)) {
     formNombre.classList.remove("is-valid");
-    formNombre.classList.add("is-invalid"); 
+    formNombre.classList.add("is-invalid");
     resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
     // El return false permite salir de la función y que resultado.innerHTML = "" no pise a los anteriores resultado.innerHTML
     return false;
@@ -24,9 +24,7 @@ function validar () {
     resultado.innerHTML = "";
   }
 
-    // validar nombre
-    console.log(formNumero.value);
-    console.log(typeof(parseInt(formNumero.value)));
+    // validar número
   if (formNumero.value.length < 10 || isNaN(parseInt(formNumero.value))) {
     formNumero.classList.remove("is-valid");
     formNumero.classList.add("is-invalid");
@@ -37,6 +35,20 @@ function validar () {
     formNumero.classList.add("is-valid");
     resultado.innerHTML = ""
   }
+
+    // Validar mail
+    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    let esValido = expReg.test(formEmail.value)
+    if (esValido == false) {
+      formEmail.classList.remove('is-valid');
+      formEmail.classList.add('is-invalid');
+      resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
+      return false;
+    } else {
+      formEmail.classList.remove('is-invalid');
+      formEmail.classList.add('is-valid');
+      resultado.innerHTML = "";
+    }
 
   // Validar mensaje
   if (formTexto.value.length < 5) {
@@ -50,30 +62,29 @@ function validar () {
     resultado.innerHTML = "";
   }
 
-  // Validar mail
-  let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-  let esValido = expReg.test(formEmail.value)
-  if (esValido == false) {
-    formEmail.classList.remove('is-valid');
-    formEmail.classList.add('is-invalid');
-    resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
-    return false;
-  } else {
-    formEmail.classList.remove('is-invalid');
-    formEmail.classList.add('is-valid');
-    resultado.innerHTML = "";
-  }
+
 }
 
 // Evento enviar
-formEnviar.addEventListener('click', () => {
-  // Actualizo los nodos cada vez que clickeo en ENVIAR
-  formNumero = document.getElementById('formNumero');
-  formEmail = document.getElementById('formEmail');
-  formTexto = document.getElementById('formTexto');
-  formCheckbox = document.getElementById('formCheckbox');
-  formEnviar = document.getElementById('formEnviar');
-  formNombre = document.getElementById('formNombre');
 
+formEnviar.addEventListener('click', () => {
+  validar();
+})
+
+// Eventos focusout
+
+formNombre.addEventListener('blur', () => {
+  validar();
+})
+
+formNumero.addEventListener('blur', () => {
+  validar();
+})
+
+formEmail.addEventListener('blur', () => {
+  validar();
+})
+
+formTexto.addEventListener('blur', () => {
   validar();
 })
