2e42ac7b1408f26562dc27c0b6fb95afd45fc62e
NicolasFontana <nico.fontana12@gmail.com>
2022-02-07 17:03:57 -0300

    Agrego animaciones con jQuery en validacionJS

--- js/validacion.js
+++ js/validacion.js
@@ -10,18 +10,18 @@ let resultado = document.getElementById('resultado');
 
 // Validar
 function validar () {
-
+  
   // validar nombre
   if ((formNombre.value == "") || (formNombre.value.length < 3)) {
     formNombre.classList.remove("is-valid");
     formNombre.classList.add("is-invalid");
     resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
+    $("#resultado").slideDown("slow");
     // El return false permite salir de la función y que resultado.innerHTML = "" no pise a los anteriores resultado.innerHTML
     return false;
   } else if (formNombre.value.length >= 3) {
     formNombre.classList.remove("is-invalid");
     formNombre.classList.add("is-valid"); 
-    resultado.innerHTML = "";
   }
 
     // validar número
@@ -29,11 +29,12 @@ function validar () {
     formNumero.classList.remove("is-valid");
     formNumero.classList.add("is-invalid");
     resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
+    $("#resultado").slideDown("slow");
     return false;
   } else {
     formNumero.classList.remove("is-invalid");
     formNumero.classList.add("is-valid");
-    resultado.innerHTML = ""
+
   }
 
     // Validar mail
@@ -43,11 +44,11 @@ function validar () {
       formEmail.classList.remove('is-valid');
       formEmail.classList.add('is-invalid');
       resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
+      $("#resultado").slideDown("slow");
       return false;
     } else {
       formEmail.classList.remove('is-invalid');
       formEmail.classList.add('is-valid');
-      resultado.innerHTML = "";
     }
 
   // Validar mensaje
@@ -55,14 +56,13 @@ function validar () {
     formTexto.classList.remove("is-valid");
     formTexto.classList.add("is-invalid");
     resultado.innerHTML = `<p class="d-flex justify-content-center mt-3 p-3 bg-danger text-white fs-3">Complete los campos solicitados porfavor</p>`;
+    $("#resultado").slideDown("slow");
     return false;
   } else {
     formTexto.classList.remove("is-invalid");
     formTexto.classList.add("is-valid");
-    resultado.innerHTML = "";
+    $("#resultado").slideUp("slow");
   }
-
-
 }
 
 // Evento enviar
@@ -73,18 +73,18 @@ formEnviar.addEventListener('click', () => {
 
 // Eventos focusout
 
-formNombre.addEventListener('blur', () => {
-  validar();
-})
+// formNombre.addEventListener('blur', () => {
+//   validar();
+// })
 
-formNumero.addEventListener('blur', () => {
-  validar();
-})
+// formNumero.addEventListener('blur', () => {
+//   validar();
+// })
 
-formEmail.addEventListener('blur', () => {
-  validar();
-})
+// formEmail.addEventListener('blur', () => {
+//   validar();
+// })
 
-formTexto.addEventListener('blur', () => {
-  validar();
-})
+// formTexto.addEventListener('blur', () => {
+//   validar();
+// })
